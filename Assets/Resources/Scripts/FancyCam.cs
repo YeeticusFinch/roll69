ca4af125028638648e626794d7e3e1038bea53ab
YeeticusFinch <50182007+YeeticusFinch@users.noreply.github.com>
2022-11-09 13:25:08 -0500

    Online Stuff

--- Assets/Resources/Scripts/FancyCam.cs
+++ Assets/Resources/Scripts/FancyCam.cs
@@ -4,13 +4,284 @@ using UnityEngine;
 
 public class FancyCam : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    float rotX = 0;
+    float rotY = 0;
+    public float mouseSensitivity = 3f;
+    bool trackSelected = false;
+
+    GameObject selected;
+    public LayerMask mask;
+
+    public TextMesh trackSelectedButton;
+    public TextMesh selectedText;
+    public TextMesh dmText;
+    public TextMesh hideText;
+    public Light flashlight;
+    public SshConnection ssh;
+
+    public TextMesh consoleText;
+
+    public bool dm = false;
+
+    string path = "Assets/Resources/Yeet.txt";
+    string serverPath = "Yeet.txt";
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetButton("Fire2"))
+        {
+            rotX -= Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+            rotY += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+            //Quaternion xQuaternion = Quaternion.AngleAxis(rotX, Vector3.up);
+            //Quaternion yQuaternion = Quaternion.AngleAxis(rotY, -Vector3.right);
+            //transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+            //transform.eulerAngles = new Vector3(rotX, rotY, 0);
+            transform.eulerAngles = new Vector3(rotX, rotY, 0);
+            //GameMaster.instance.board.canMove = false;
+        }
+
+        if (!dm || selected == null)
+        {
+            hideText.text = "";
+        } else
+        {
+            hideText.text = "Hide = " + (selected.GetComponent<FancyObject>().hide ? "true" : "false");
+        }
+
+        if (Input.GetButtonDown("Flashlight"))
+        {
+            if (flashlight.intensity == 0)
+                flashlight.intensity = 1;
+            else
+                flashlight.intensity = 0;
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000, mask))
+            {
+                //Debug.Log(hit.transform.gameObject.name);
+                if (hit.transform.GetComponent<TextMesh>() != null)
+                {
+                    StartCoroutine(ClickAnim(hit.transform.GetComponent<TextMesh>()));
+                }
+                if (hit.transform.GetComponent<FancyObject>() != null && (dm || !hit.transform.GetComponent<FancyObject>().dm_only))
+                {
+                    Debug.Log("Selection change");
+                    selected = hit.transform.gameObject;
+                    selectedText.text = "Selected = " + hit.transform.gameObject.GetComponent<FancyObject>().title;
+                }
+                else if (hit.transform.gameObject.name.Equals("Change Sprite"))
+                {
+                    Debug.Log("Changing sprite of selected object");
+                    if (selected != null)
+                    {
+                        selected.GetComponent<FancyObject>().NextSprite();
+                    }
+                }
+                else if (hit.transform.gameObject.name.Equals("First Person"))
+                {
+                    Debug.Log("Camera to Selected");
+                    if (selected != null)
+                    {
+                        this.transform.SetPositionAndRotation(selected.transform.position, Quaternion.Euler(selected.GetComponent<FancyObject>().modelEulers));
+                    }
+                }
+                else if (hit.transform.gameObject.name.Equals("Selected to Camera"))
+                {
+                    Debug.Log("Selected to Camera");
+                    if (selected != null)
+                    {
+                        selected.transform.SetPositionAndRotation(this.transform.position, Quaternion.Euler(selected.GetComponent<FancyObject>().modelEulers));
+                    }
+                }
+                else if (hit.transform.gameObject.name.Equals("Track Selected"))
+                {
+                    Debug.Log("Toggle track selected object");
+                    if (selected != null)
+                    {
+                        trackSelected = !trackSelected;
+                        trackSelectedButton.text = "Track Selected = " + (trackSelected ? "true" : "false");
+                    }
+                }
+                else if (hit.transform.gameObject.name.Equals("DM Text"))
+                {
+                    Debug.Log("DM login attempt");
+                    if (ssh.tryLogin())
+                    {
+                        dm = !dm;
+                        dmText.text = "DM = " + (dm ? "true" : "false");
+                        GameObject[] fancyObjects = GameObject.FindGameObjectsWithTag("FancyObject");
+                        foreach (GameObject o in fancyObjects)
+                        {
+                            o.GetComponent<FancyObject>().UpdateDisplay();
+                        }
+                    }
+                }
+                else if (hit.transform.gameObject.name.Equals("Pull"))
+                {
+                    StartCoroutine(pullFromServer());
+                }
+                else if (hit.transform.gameObject.name.Equals("Push"))
+                {
+                    StartCoroutine(pushToServer());
+                }
+                else if (hit.transform.gameObject.name.Equals("Hide"))
+                {
+                    Debug.Log("Toggle visibility");
+                    if (selected != null)
+                    {
+                        selected.GetComponent<FancyObject>().hide = !selected.GetComponent<FancyObject>().hide;
+                        selected.GetComponent<FancyObject>().UpdateDisplay();
+                    }
+                }
+            }
+        }
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Jump") != 0) {
+            if (Input.GetButton("Move Selected"))
+            {
+                // move the selected piece
+                if (selected != null)
+                {
+                    selected.transform.position += (Input.GetAxisRaw("Vertical") * horizontalForward() + Input.GetAxisRaw("Horizontal") * horizontalRight() + Input.GetAxisRaw("Jump") * Vector3.up) * (0.6f + 0.5f*Input.GetAxisRaw("Sprint"));
+                    if (trackSelected)
+                        transform.position += (Input.GetAxisRaw("Vertical") * horizontalForward() + Input.GetAxisRaw("Horizontal") * horizontalRight() + Input.GetAxisRaw("Jump") * Vector3.up) * (0.6f + 0.5f * Input.GetAxisRaw("Sprint"));
+                }
+            } else
+            {
+                //move the camera
+                transform.position += (Input.GetAxisRaw("Vertical") * transform.forward + Input.GetAxisRaw("Horizontal") * transform.right + Input.GetAxisRaw("Jump") * transform.up) * (0.6f + 0.5f*Input.GetAxisRaw("Sprint"));
+            }
+        }
+    }
+
+    IEnumerator pullFromFile()
+    {
+        yield return new WaitForEndOfFrame();
+        GameObject[] fancyObjects = GameObject.FindGameObjectsWithTag("FancyObject");
+        Debug.Log("Reading from " + path);
+        System.IO.StreamReader reader = new System.IO.StreamReader(path);
+
+        string m = reader.ReadToEnd();
+        reader.Close();
+
+        deserialize(m);
+    }
+
+    IEnumerator pullFromServer()
+    {
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Reading from server");
+
+        //deserialize(ssh.readFromFile(serverPath));
+        ssh.pullFile = serverPath;
+        while (ssh.pullContents == null && ssh.connection)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Debug.Log("Waiting for response");
+        }
+        if (ssh.pullContents != null)
+        {
+            Debug.Log("Found response");
+            deserialize(ssh.pullContents);
+        }
+    }
+
+    void deserialize(string m)
+    {
+        GameObject[] fancyObjects = GameObject.FindGameObjectsWithTag("FancyObject");
+        int p = 0;
+        while (p < m.Length && m.IndexOf("}", p) != -1 && m.IndexOf("{", p) != -1)
+        {
+            p = m.IndexOf("{", p) + 1;
+            string id = m.Substring(m.IndexOf("id:", p) + 3, m.IndexOf(";", p) - (m.IndexOf("id:", p) + 3));
+            foreach (GameObject o in fancyObjects)
+            {
+                if (o.GetComponent<FancyObject>().id.Equals(id))
+                {
+                    o.GetComponent<FancyObject>().Deserialize(m.Substring(p, m.IndexOf("}", p) - p));
+                    break;
+                }
+            }
+        }
+    }
+
+    IEnumerator pushToFile()
+    {
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Writing to " + path);
+       
+        System.IO.File.WriteAllText(path, serialize());
+    }
+
+    IEnumerator pushToServer()
+    {
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Writing to server");
+
+        //ssh.writeToFile(serverPath, serialize());
+        ssh.pushFile = serverPath;
+        ssh.pushContents = serialize();
+    }
+
+    string serialize()
+    {
+        string fancyText = "";
+        GameObject[] fancyObjects = GameObject.FindGameObjectsWithTag("FancyObject");
+        foreach (GameObject o in fancyObjects)
+        {
+            fancyText += "{" + o.GetComponent<FancyObject>().Serialize() + "}";
+        }
+        if (System.IO.File.Exists(path))
+        {
+            System.IO.File.Delete(path);
+        }
+        return fancyText;
+    }
+
+    Vector3 horizontalForward()
+    {
+        return new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
+    }
+
+    Vector3 horizontalRight()
+    {
+        return Vector3.Cross(horizontalForward(), -Vector3.up);
+        //return new Vector3(transform.right.x, 0, transform.right.y).normalized;
+    }
+
+    /* // dropbox fallback
+    IEnumerator upload()
+    {
+        Dictionary<string, string> postHeader = new Dictionary<string, string>();
+        postHeader.Add("Authorization", "Bearer <code>");
+        postHeader.Add("Dropbox-API-Arg", "{\"path\": \"/provaupload.json\"}");
+        postHeader.Add("Content-Type", "");
+        using (WWW www = new WWW("https://content.dropboxapi.com/2/files/download", null, postHeader))
+        {
+            yield return www;
+            if (www.error != null)
+            {
+                Debug.Log(www.error + " " + www.text);
+            }
+            else
+            {
+                Debug.Log("Success! " + www.text);
+            }
+        }
+    }*/
+
+    IEnumerator ClickAnim(TextMesh text)
+    {
+        Color ogColor = text.color;
+        text.color = new Color(1 - ogColor.r, 1 - ogColor.g, 1 - ogColor.b);
+        yield return new WaitForSeconds(0.2f);
+        text.color = ogColor;
+    }
 }
--- Assets/Resources/Scripts/FancyObject.cs
+++ Assets/Resources/Scripts/FancyObject.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+//using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -7,46 +8,197 @@ using UnityEngine.UI;
 public class FancyObject : NetworkBehaviour
 {
 
-    public string name;
-    public bool flat = true;
+    public string title;
+    public bool flat = false;
     public string owner;
     public int hp;
     public int ac;
     public string id;
     public long timestamp;
-    public string display;
+    public string sprite0;
+    public string sprite1;
+    public string sprite2;
+    public string sprite3;
+    public string sprite4;
+    public string sprite5;
+    public string model0;
+    public string model1;
+    public string model2;
+    public string model3;
+    public Vector3 modelEulers;
+    public Sprite[] sprites = new Sprite[6];
+    public GameObject[] models = new GameObject[4];
 
-    GameObject model;
+    public GameObject display;
+    public int currentDisplay;
+    public bool dm_only = true;
+    public bool hide = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        UpdateDisplay();
+        modelEulers = Vector3.zero;
+    }
 
+    public void UpdateDisplay()
+    {
+        if (hide)
+        {
+            if (GameObject.FindGameObjectWithTag("MainCamera").gameObject.GetComponent<FancyCam>().dm)
+            {
+                GetComponent<SpriteRenderer>().color = new Color(0.2f, 0.2f, 0.2f);
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().sprite = null;
+                if (display != null)
+                {
+                    GameObject.Destroy(display);
+                    display = null;
+                }
+                return;
+
+            }
+        } else
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        if (currentDisplay < sprites.Length)
+        {
+            if (display != null)
+            {
+                GameObject.Destroy(display);
+                display = null;
+            }
+            this.GetComponent<SpriteRenderer>().sprite = GetSprite(currentDisplay);
+            //Debug.Log(GetSprite(currentDisplay));
+        } else
+        {
+            GetComponent<SpriteRenderer>().sprite = null;
+            if (display != null)
+            {
+                GameObject.Destroy(display);
+            }
+            display = GameObject.Instantiate(GetModel(currentDisplay - sprites.Length), transform.position, Quaternion.identity);
+            display.transform.SetParent(transform);
+        }
+    }
+
+    public void NextSprite()
+    {
+        currentDisplay++;
+        while (GetSprite(currentDisplay) == null && GetModel(currentDisplay - sprites.Length) == null)
+        {
+            currentDisplay++;
+            currentDisplay %= sprites.Length + models.Length;
+        }
+        Debug.Log("Switching to " + currentDisplay + " which is " + GetSprite(currentDisplay) + " and " + GetModel(currentDisplay));
+        UpdateDisplay();
+    }
+
+    Sprite GetSprite(int n)
+    {
+        if (n >= sprites.Length)
+            return null;
+        if (sprites[n] == null)
+        {
+            sprites[n] = Resources.Load(GetDisplay(n), typeof(Sprite)) as Sprite;
+        }
+        
+        return sprites[n];
+    }
+
+    GameObject GetModel(int n)
+    {
+        if (n >= models.Length || n < 0)
+            return null;
+        else if (models[n] == null)
+        {
+            models[n] = Resources.Load(GetDisplay(sprites.Length + n)) as GameObject;
+        }
+        return models[n];
+    }
+
+    string GetDisplay(int n)
+    {
+        string result = null;
+        int c = 0;
+        do
+        {
+            c++;
+            switch (n)
+            {
+                case 0:
+                    result = sprite0;
+                    break;
+                case 1:
+                    result = sprite1;
+                    break;
+                case 2:
+                    result = sprite2;
+                    break;
+                case 3:
+                    result = sprite3;
+                    break;
+                case 4:
+                    result = sprite4;
+                    break;
+                case 5:
+                    result = sprite5;
+                    break;
+                case 6:
+                    result = model0;
+                    break;
+                case 7:
+                    result = model1;
+                    break;
+                case 8:
+                    result = model2;
+                    break;
+                case 9:
+                    result = model3;
+                    break;
+            }
+            if (c > 10) break;
+        } while (result == null || result.Equals(""));
+        return result;
     }
 
     void InitNew()
     {
-        id = CarlMath.RandomString(Random.Range(6, 10));
+        id = CarlMath.RandomString(Random.Range(6, 10)).Replace(';', 'l').Replace(':','k').Replace('{','j').Replace('}','h');
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!flat && currentDisplay < sprites.Length)
+        {
+            //transform.LookAt(GameObject.FindGameObjectWithTag("MainCamera").transform);
+            transform.eulerAngles = GameObject.FindGameObjectWithTag("MainCamera").transform.eulerAngles;
+        }
     }
 
-    void Deserialize(string input)
+    public void Deserialize(string input)
     {
         int i = 0;
-        while (i < input.Length)
+        while (i < input.Length && input.IndexOf(':', i) != -1)
         {
-            string sel = input.Substring(i, input.IndexOf(':') - i);
+            /*
+            string sel = input.Substring(i, input.IndexOf(':', i) - i);
             if (sel[0] == ';') sel = sel.Substring(1);
             if (sel[sel.Length - 1] == ':') sel = sel.Substring(0, sel.Length - 1);
-            string val = input.Substring(input.IndexOf(':') + 1, input.IndexOf(';', i + 2) - (input.IndexOf(':') + 1));
+            string val = input.Substring(input.IndexOf(':', i) + 1, input.IndexOf(';', i + 1) - (input.IndexOf(':', i) + 1));
             if (val[0] == ':') sel = val.Substring(1);
             if (val[sel.Length - 1] == ';') val = sel.Substring(0, sel.Length - 1);
             i = input.IndexOf(';', i + 2) + 1;
+            */
+            string sel = input.Substring(i, input.IndexOf(':', i) - i);
+            if (sel[0] == ';') sel = sel.Substring(1);
+            if (sel[sel.Length - 1] == ':') sel = sel.Substring(0, sel.Length - 1);
+            string val = input.Substring(input.IndexOf(':', i) + 1, input.IndexOf(';', i) - (input.IndexOf(':', i) + 1));
+            i = input.IndexOf(';', i)+1;
             switch (sel)
             {
                 case "id":
@@ -65,6 +217,7 @@ public class FancyObject : NetworkBehaviour
                     owner = val;
                     break;
                 case "hp":
+                    Debug.Log("HP = " + val);
                     hp = int.Parse(val);
                     break;
                 case "ac":
@@ -100,16 +253,129 @@ public class FancyObject : NetworkBehaviour
                 case "timestamp":
                     timestamp = long.Parse(val);
                     break;
-                case "display":
-                    display = val;
+                case "sprite0":
+                    if (sprite0 == null || sprite0.Equals(""))
+                        break;
+                    sprite0 = val;
+                    sprites[0] = Resources.Load(val) as Sprite;
+                    break;
+                case "sprite1":
+                    if (sprite1 == null || sprite1.Equals(""))
+                        break;
+                    sprite1 = val;
+                    sprites[1] = Resources.Load(val) as Sprite;
+                    break;
+                case "sprite2":
+                    if (sprite2 == null || sprite2.Equals(""))
+                        break;
+                    sprite2 = val;
+                    sprites[2] = Resources.Load(val) as Sprite;
+                    break;
+                case "sprite3":
+                    if (sprite3 == null || sprite3.Equals(""))
+                        break;
+                    sprite3 = val;
+                    sprites[3] = Resources.Load(val) as Sprite;
+                    break;
+                case "sprite4":
+                    if (sprite4 == null || sprite4.Equals(""))
+                        break;
+                    sprite4 = val;
+                    sprites[4] = Resources.Load(val) as Sprite;
+                    break;
+                case "sprite5":
+                    if (sprite5 == null || sprite5.Equals(""))
+                        break;
+                    sprite5 = val;
+                    sprites[5] = Resources.Load(val) as Sprite;
+                    break;
+                case "model0":
+                    if (model0 == null || model0.Equals(""))
+                        break;
+                    model0 = val;
+                    models[0] = Resources.Load(val) as GameObject;
+                    break;
+                case "model1":
+                    if (model1 == null || model1.Equals(""))
+                        break;
+                    model1 = val;
+                    models[1] = Resources.Load(val) as GameObject;
+                    break;
+                case "model2":
+                    if (model2 == null || model2.Equals(""))
+                        break;
+                    model2 = val;
+                    models[2] = Resources.Load(val) as GameObject;
+                    break;
+                case "model3":
+                    if (model3 == null || model3.Equals(""))
+                        break;
+                    model3 = val;
+                    models[3] = Resources.Load(val) as GameObject;
+                    break;
+                case "currentDisplay":
+                    currentDisplay = int.Parse(val);
+                    break;
+                case "hide":
+                    hide = bool.Parse(val);
                     break;
             }
+            UpdateDisplay();
         }
     }
 
-    string Serialize()
+    public string Serialize()
     {
-        return "id:" + id + ";name:" + name + ";flat:" + (flat ? 1 : 0) + ";owner:" + owner + ";hp:" + hp + ";ac:" + ac + ";xpos:" + transform.position.x + ";ypos:" + transform.position.y + ";zpos:" + transform.position.z + ";xsiz:" + transform.localScale.x + ";ysiz:" + transform.localScale.y + ";zsiz:" + transform.localScale.z + ";xeul:" + transform.eulerAngles.x + ";yeul:" + transform.eulerAngles.y + ";zeul:" + transform.eulerAngles.z + ";timestamp:" + System.DateTimeOffset.Now.ToUnixTimeMilliseconds() + ";display:" + display + ";";
+        return "id:" + id 
+            + ";name:" + name 
+            + ";flat:" + (flat ? 1 : 0) 
+            + ";owner:" + owner 
+            + ";hp:" + hp 
+            + ";ac:" + ac 
+            + ";xpos:" + transform.position.x 
+            + ";ypos:" + transform.position.y 
+            + ";zpos:" + transform.position.z 
+            + ";xsiz:" + transform.localScale.x 
+            + ";ysiz:" + transform.localScale.y 
+            + ";zsiz:" + transform.localScale.z 
+            + ";xeul:" + transform.eulerAngles.x 
+            + ";yeul:" + transform.eulerAngles.y 
+            + ";zeul:" + transform.eulerAngles.z 
+            + ";timestamp:" + System.DateTimeOffset.Now.ToUnixTimeMilliseconds() 
+            + ";sprite0:" + sprite0 
+            + ";sprite1:" + sprite1 
+            + ";sprite2:" + sprite2 
+            + ";sprite3:" + sprite3 
+            + ";sprite4:" + sprite4 
+            + ";sprite5:" + sprite5 
+            + ";model0:" + model0 
+            + ";model1:" + model1 
+            + ";model2:" + model2 
+            + ";model3:" + model3
+            + ";currentDisplay:" + currentDisplay
+            + ";hide:" + (hide ? "true" : "false")
+            + ";";
     }
 
+    /*
+    [MenuItem("Fancy/Find Sprite")]
+    public static void FindSprite()
+    {
+        var selected = Selection.activeGameObject;
+        if (selected == null) return;
+        var renderer = selected.GetComponent<SpriteRenderer>();
+        if (renderer == null) return;
+        Debug.Log(AssetDatabase.GetAssetPath(renderer.sprite));
+    }
+
+    [MenuItem("Fancy/Find Model")]
+    public static void FindModel()
+    {
+        var selected = Selection.activeGameObject;
+        if (selected == null) return;
+        var renderer = selected.GetComponent<MeshRenderer>();
+        if (renderer == null) return;
+        Debug.Log(AssetDatabase.GetAssetPath(renderer.gameObject));
+    }
+    */
 }
